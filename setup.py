40b162c8a554565f99412c77bd51c27636d127ce
Michael Ben-Ami <mike.benami@gmail.com>
2016-03-14 17:55:36 -0400

    version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 import sys
 
 name = 'pyntc'
-version = '0.0.2'
+version = '0.0.3'
 packages = find_packages()
 package_data = {'pyntc': ['templates/*.template', 'devices/tables/jnpr/*.yml']}
 
