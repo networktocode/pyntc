a38bb611f2ef592178fb70b007a028248cd514e0
Mikhail Yohman <mikhail.yohman@gmail.com>
2018-06-04 21:56:54 -0600

    Fixed the save function #2

--- pyntc/devices/ios_device.py
+++ pyntc/devices/ios_device.py
@@ -125,7 +125,7 @@ class IOSDevice(BaseDevice):
         command = 'copy running-config %s' % filename
         # Changed to send_command_timing to not require a direct prompt return.
         self.native.send_command_timing(command)
-        # If the user has enabled 'file prompt quiet' which dose not require any confirmation or feedback. This will send return wit        hout requiring an OK.
+        # If the user has enabled 'file prompt quiet' which dose not require any confirmation or feedback. This will send return without requiring an OK.
         # Send a return to pass the [OK]? message - Incease delay_factor for looking for response.
         self.native.send_command_timing('\n',delay_factor=2)
         # Confirm that we have a valid prompt again before returning.
