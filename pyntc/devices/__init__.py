3ba235ed2307e0b7fd6f91fbd0a4938e769dd57d
Marek Zbroch <marek@zbroch.com>
2018-07-09 12:34:44 +0200

    Device Type Fix

--- pyntc/devices/__init__.py
+++ pyntc/devices/__init__.py
@@ -6,6 +6,7 @@ from .eos_device import EOSDevice, EOS_API_DEVICE_TYPE
 from .nxos_device import NXOSDevice, NXOS_API_DEVICE_TYPE
 from .ios_device import IOSDevice, IOS_SSH_DEVICE_TYPE
 from .jnpr_device import JunosDevice, JNPR_DEVICE_TYPE
+from .f5_device import F5Device, F5_API_DEVICE_TYPE
 from .base_device import BaseDevice
 
 
@@ -27,5 +28,8 @@ supported_devices = {
 
     JNPR_DEVICE_TYPE: {
         DEVICE_CLASS_KEY: JunosDevice,
+    },
+    F5_DEVICE_TYPE: {
+        DEVICE_CLASS_KEY: F5Device,
     },
 }
