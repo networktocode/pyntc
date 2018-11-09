20f7b7bb3af76befc54f19591e7ee479ef765657
jmcgill298 <jacob@networktocode.com>
2018-11-09 16:07:29 -0500

    IOSDevice: Add `install_os` method

--- pyntc/devices/ios_device.py
+++ pyntc/devices/ios_device.py
@@ -242,6 +242,38 @@ class IOSDevice(BaseDevice):
 
         return {'sys': boot_image}
 
+    def install_os(self, image_name, **vendor_specifics):
+        dest = vendor_specifics.get("dest")
+        file_system = vendor_specifics.get("file_system")
+        timeout = vendor_specifics.get("timeout", 3600)
+        if not self._image_booted(image_name):
+            # TODO: Just make call to file_copy if/when file_copy PR is merged
+            if not self.file_copy_remote_exists(image_name, dest, file_system):
+                self.file_copy(image_name, dest, file_system)
+
+            self.set_boot_options(image_name)
+            # TODO: Do validation in set_boot_options and remove from here
+            if not self._image_booted(image_name, dest=dest, file_system=file_system):
+                raise CommandError(
+                    command="boot command",
+                    message="Setting boot command did not yield expected results",
+                )
+
+            self.save()
+            self.reboot(confirm=True)
+            self._reconnect(timeout=timeout)
+            if not self._image_booted(image_name, dest=dest, file_system=file_system):
+                # TODO: Raise proper exception class
+                raise ValueError(
+                    "Image was not what was expected after reboot. "
+                    "Current image: {0}"
+                    "Expected image: {1}".format(self.get_boot_options(), image_name)
+                )
+
+            return True
+
+        return False
+
     def open(self):
         if self._connected:
             try:
