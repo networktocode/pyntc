6389b41bb9c007ca8e79b9b90e99e0184711b91a
amb1s1 <amb1s1@gmail.com>
2018-11-20 18:26:40 -0500

    replace single quotes with double quotes and reformat for pep8 for ios key map in data model

--- pyntc/data_model/key_maps/ios_key_maps.py
+++ pyntc/data_model/key_maps/ios_key_maps.py
@@ -1,8 +1,3 @@
 # Key maps for IOS devices are stored here.
 
-BASIC_FACTS_KM = {
-    'model': 'hardware',
-    'os_version': 'version',
-    'serial_number': 'serial',
-    'hostname': 'hostname',
-}
+BASIC_FACTS_KM = {"model": "hardware", "os_version": "version", "serial_number": "serial", "hostname": "hostname"}
