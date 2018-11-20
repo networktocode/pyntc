6587c27c89416965eaa82ebfe7f5968d9a710c7c
amb1s1 <amb1s1@gmail.com>
2018-11-20 18:26:24 -0500

    replace single quotes with double quotes and reformat for pep8 for eos key map in data model

--- pyntc/data_model/key_maps/eos_key_maps.py
+++ pyntc/data_model/key_maps/eos_key_maps.py
@@ -1,22 +1,14 @@
 # Key maps for EOS devices are stored here.
 
-BASIC_FACTS_KM = {
-    'model': 'modelName',
-    'os_version': 'internalVersion',
-    'serial_number': 'serialNumber'
-}
+BASIC_FACTS_KM = {"model": "modelName", "os_version": "internalVersion", "serial_number": "serialNumber"}
 
 
 INTERFACES_KM = {
-    'speed': 'bandwidth',
-    'duplex': 'duplex',
-    'vlan': ['vlanInformation', 'vlanId'],
-    'state': 'linkStatus',
-    'description': 'description',
+    "speed": "bandwidth",
+    "duplex": "duplex",
+    "vlan": ["vlanInformation", "vlanId"],
+    "state": "linkStatus",
+    "description": "description",
 }
 
-VLAN_KM = {
-    'state': 'state',
-    'name': 'name',
-    'id': 'vlan_id',
-}
+VLAN_KM = {"state": "state", "name": "name", "id": "vlan_id"}
