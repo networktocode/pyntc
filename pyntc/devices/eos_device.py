cc060027cb396b8c0ce43e2bac6fdbc7dadf7f76
amb1s1 <amb1s1@gmail.com>
2018-11-20 18:17:48 -0500

    replace single quotes with double quotes and reformat for pep8 for eos devices

--- pyntc/devices/eos_device.py
+++ pyntc/devices/eos_device.py
@@ -4,8 +4,7 @@
 import re
 import time
 
-from pyntc.data_model.converters import convert_dict_by_key, \
-    convert_list_by_key, strip_unicode
+from pyntc.data_model.converters import convert_dict_by_key, convert_list_by_key, strip_unicode
 from pyntc.data_model.key_maps import eos_key_maps
 from .system_features.file_copy.eos_file_copy import EOSFileCopy
 from .system_features.vlans.eos_vlans import EOSVlans
@@ -28,9 +27,8 @@ from .system_features.file_copy.base_file_copy import FileTransferError
 
 @fix_docs
 class EOSDevice(BaseDevice):
-
-    def __init__(self, host, username, password, transport='http', timeout=60, **kwargs):
-        super(EOSDevice, self).__init__(host, username, password, vendor='arista', device_type='arista_eos_eapi')
+    def __init__(self, host, username, password, transport="http", timeout=60, **kwargs):
+        super(EOSDevice, self).__init__(host, username, password, vendor="arista", device_type="arista_eos_eapi")
         self.transport = transport
         self.timeout = timeout
         self.connection = eos_connect(transport, host=host, username=username, password=password, timeout=timeout)
@@ -47,14 +45,14 @@ class EOSDevice(BaseDevice):
         """
         raw_data = self.show("dir", raw_text=True)
         try:
-            file_system = re.match(r'\s*.*?(\S+:)', raw_data).group(1)
+            file_system = re.match(r"\s*.*?(\S+:)", raw_data).group(1)
             return file_system
         except AttributeError:
             raise FileSystemNotFoundError(hostname=self.facts.get("hostname"), command="dir")
 
     def _get_interface_list(self):
         iface_detailed_list = self._interfaces_status_list()
-        iface_list = sorted(list(x['interface'] for x in iface_detailed_list))
+        iface_list = sorted(list(x["interface"] for x in iface_detailed_list))
 
         return iface_list
 
@@ -73,20 +71,19 @@ class EOSDevice(BaseDevice):
 
     def _interfaces_status_list(self):
         interfaces_list = []
-        interfaces_status_dictionary = self.show(
-            'show interfaces status')['interfaceStatuses']
+        interfaces_status_dictionary = self.show("show interfaces status")["interfaceStatuses"]
         for key in interfaces_status_dictionary:
             interface_dictionary = interfaces_status_dictionary[key]
-            interface_dictionary['interface'] = key
+            interface_dictionary["interface"] = key
             interfaces_list.append(interface_dictionary)
 
-        return convert_list_by_key(interfaces_list, eos_key_maps.INTERFACES_KM, fill_in=True, whitelist=['interface'])
+        return convert_list_by_key(interfaces_list, eos_key_maps.INTERFACES_KM, fill_in=True, whitelist=["interface"])
 
     def _parse_response(self, response, raw_text):
         if raw_text:
-            return list(x['result']['output'] for x in response)
+            return list(x["result"]["output"] for x in response)
         else:
-            return list(x['result'] for x in response)
+            return list(x["result"] for x in response)
 
     def _uptime_to_string(self, uptime):
         days = uptime / (24 * 60 * 60)
@@ -100,7 +97,7 @@ class EOSDevice(BaseDevice):
 
         seconds = uptime
 
-        return '%02d:%02d:%02d:%02d' % (days, hours, mins, seconds)
+        return "%02d:%02d:%02d:%02d" % (days, hours, mins, seconds)
 
     def _wait_for_device_reboot(self, timeout=3600):
         start = time.time()
@@ -114,11 +111,11 @@ class EOSDevice(BaseDevice):
         raise RebootTimeoutError(hostname=self.facts["hostname"], wait_time=timeout)
 
     def backup_running_config(self, filename):
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             f.write(self.running_config)
 
     def checkpoint(self, checkpoint_file):
-        self.show('copy running-config %s' % checkpoint_file)
+        self.show("copy running-config %s" % checkpoint_file)
 
     def close(self):
         pass
@@ -137,28 +134,24 @@ class EOSDevice(BaseDevice):
 
     @property
     def facts(self):
-        if hasattr(self, '_facts'):
+        if hasattr(self, "_facts"):
             return self._facts
 
         facts = {}
-        facts['vendor'] = self.vendor
+        facts["vendor"] = self.vendor
 
-        sh_version_output = self.show('show version')
-        facts.update(
-            convert_dict_by_key(
-                sh_version_output, eos_key_maps.BASIC_FACTS_KM))
+        sh_version_output = self.show("show version")
+        facts.update(convert_dict_by_key(sh_version_output, eos_key_maps.BASIC_FACTS_KM))
 
-        uptime = int(time.time() - sh_version_output['bootupTimestamp'])
-        facts['uptime'] = uptime
-        facts['uptime_string'] = self._uptime_to_string(uptime)
+        uptime = int(time.time() - sh_version_output["bootupTimestamp"])
+        facts["uptime"] = uptime
+        facts["uptime_string"] = self._uptime_to_string(uptime)
 
-        sh_hostname_output = self.show('show hostname')
-        facts.update(
-            convert_dict_by_key(
-                sh_hostname_output, {}, fill_in=True, whitelist=['hostname', 'fqdn']))
+        sh_hostname_output = self.show("show hostname")
+        facts.update(convert_dict_by_key(sh_hostname_output, {}, fill_in=True, whitelist=["hostname", "fqdn"]))
 
-        facts['interfaces'] = self._get_interface_list()
-        facts['vlans'] = self._get_vlan_list()
+        facts["interfaces"] = self._get_interface_list()
+        facts["vlans"] = self._get_vlan_list()
 
         self._facts = facts
         return facts
@@ -170,8 +163,7 @@ class EOSDevice(BaseDevice):
 
             if not self.file_copy_remote_exists(src, dest, **kwargs):
                 raise FileTransferError(
-                    message="Attempted file copy, "
-                            "but could not validate file existed after transfer"
+                    message="Attempted file copy, " "but could not validate file existed after transfer"
                 )
 
     # TODO: Make this an internal method since exposing file_copy should be sufficient
@@ -182,8 +174,8 @@ class EOSDevice(BaseDevice):
         return False
 
     def get_boot_options(self):
-        image = self.show('show boot-config')['softwareImage']
-        image = image.replace('flash:', '')
+        image = self.show("show boot-config")["softwareImage"]
+        image = image.replace("flash:", "")
         return dict(sys=image)
 
     def open(self):
@@ -194,22 +186,22 @@ class EOSDevice(BaseDevice):
             raise RebootTimerError(self.device_type)
 
         if confirm:
-            self.show('reload now')
+            self.show("reload now")
         else:
-            print('Need to confirm reboot with confirm=True')
+            print("Need to confirm reboot with confirm=True")
 
     def rollback(self, rollback_to):
         try:
-            self.show('configure replace %s force' % rollback_to)
+            self.show("configure replace %s force" % rollback_to)
         except (CommandError, CommandListError):
-            raise RollbackError('Rollback unsuccessful. %s may not exist.' % rollback_to)
+            raise RollbackError("Rollback unsuccessful. %s may not exist." % rollback_to)
 
     @property
     def running_config(self):
-        return self.show('show running-config', raw_text=True)
+        return self.show("show running-config", raw_text=True)
 
-    def save(self, filename='startup-config'):
-        self.show('copy running-config %s' % filename)
+    def save(self, filename="startup-config"):
+        self.show("copy running-config %s" % filename)
         return True
 
     def set_boot_options(self, image_name, **vendor_specifics):
@@ -219,11 +211,9 @@ class EOSDevice(BaseDevice):
 
         file_system_files = self.show("dir {0}".format(file_system), raw_text=True)
         if re.search(image_name, file_system_files) is None:
-            raise NTCFileNotFoundError(
-                hostname=self.facts.get("hostname"), file=image_name, dir=file_system
-            )
+            raise NTCFileNotFoundError(hostname=self.facts.get("hostname"), file=image_name, dir=file_system)
 
-        self.show('install source {0}{1}'.format(file_system, image_name))
+        self.show("install source {0}{1}".format(file_system, image_name))
         if self.get_boot_options()["sys"] != image_name:
             raise CommandError(
                 command="install source {0}".format(image_name),
@@ -239,19 +229,20 @@ class EOSDevice(BaseDevice):
 
     def show_list(self, commands, raw_text=False):
         if raw_text:
-            encoding = 'text'
+            encoding = "text"
         else:
-            encoding = 'json'
+            encoding = "json"
 
         try:
             return strip_unicode(
-                self._parse_response(self.native.enable(commands, encoding=encoding), raw_text=raw_text))
+                self._parse_response(self.native.enable(commands, encoding=encoding), raw_text=raw_text)
+            )
         except EOSCommandError as e:
             raise CommandListError(commands, e.commands[len(e.commands) - 1], e.message)
 
     @property
     def startup_config(self):
-        return self.show('show startup-config', raw_text=True)
+        return self.show("show startup-config", raw_text=True)
 
 
 class RebootSignal(NTCError):
