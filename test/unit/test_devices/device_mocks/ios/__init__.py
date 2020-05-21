3cb7849e1da028d8552eacd64b79c0cf92bb0c17
mundruid <mountrouidoux@cofc.edu>
2020-05-21 18:14:08 -0400

    make error message more descriptive

--- test/unit/test_devices/device_mocks/ios/__init__.py
+++ test/unit/test_devices/device_mocks/ios/__init__.py
@@ -1,20 +1,24 @@
 import os
 import re
 
-CURRNENT_DIR = os.path.dirname(os.path.realpath(__file__))
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 
 def send_command(command, **kwargs):
+    """
+    TODO: Document me
+    """
+    orig = command
     command = command.replace(" ", "_")
     command = command.replace("/", "_")
 
     if command == "\n":
         command = "return"
 
-    path = os.path.join(CURRNENT_DIR, "send_command", command)
+    path = os.path.join(CURRENT_DIR, "send_command", command)
 
     if not os.path.isfile(path):
-        return "% Error: mock error"
+        return f"% Error: Cannot find mock: {orig}"
 
     with open(path, "r") as f:
         response = f.read()
