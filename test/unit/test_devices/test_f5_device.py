24402eea11343fc966008e486c10c58d90486c73
Dr.X <mountrouidoux@cofc.edu>
2020-06-23 10:18:53 -0400

    Added test_file_copy_no_dest

--- test/unit/test_devices/test_f5_device.py
+++ test/unit/test_devices/test_f5_device.py
@@ -53,17 +53,33 @@ class TestF5Device:
         name = "./test/unit/test_devices/device_mocks/f5/send_command/source_file"
         self.device.file_copy(name, "/shared/images/dest_file")
 
+        # Check if _check_free_space worked
         api.tm.util.bash.exec_cmd.assert_called_with("run", utilCmdArgs='-c "vgdisplay -s --units G"')
+        # Check if _upload_image REST API request worked
         URI = "https://host/mgmt/cm/autodeploy/software-image-uploads/source_file"
         data = b"Space, the final fronteer..."
         headers = {"Content-Type": "application/octet-stream", "Content-Range": "0-27/28"}
         mock_post.assert_called_with(URI, auth=("user", "password"), data=data, headers=headers, verify=False)
 
-    # @mock.patch.object(F5Device, "file_copy_remote_exists", side_effect=[False, True])
-    # def test_file_copy_no_dest(self, mock_fcre):
-    #     self.device.file_copy("source_file")
-    #     self.device.native.file_copy.assert_called_with("source_file", "source_file", file_system=FILE_SYSTEM)
-    #     self.device.native.file_copy.assert_called()
+    @mock.patch.object(F5Device, "file_copy_remote_exists", side_effect=[False, True])
+    @mock.patch("requests.post")
+    def test_file_copy_no_dest(self, mock_post, mock_fcre):
+        # Pull ManagementRoot mock instance off device
+        api = self.device.api_handler
+        # Patching out the __get_free_space API call internal
+        api.tm.util.bash.exec_cmd.return_value.commandResult = '"vg-db-sda" 30.98 GB  [23.89 GB  used / 7.10 GB free]'
+
+        name = "./test/unit/test_devices/device_mocks/f5/send_command/source_file"
+        # the only difference with file_copy is that here we are testing with same source and dest file
+        self.device.file_copy(name, "/shared/images/source_file")
+
+        # Check if _check_free_space worked
+        api.tm.util.bash.exec_cmd.assert_called_with("run", utilCmdArgs='-c "vgdisplay -s --units G"')
+        # Check if _upload_image REST API request worked
+        URI = "https://host/mgmt/cm/autodeploy/software-image-uploads/source_file"
+        data = b"Space, the final fronteer..."
+        headers = {"Content-Type": "application/octet-stream", "Content-Range": "0-27/28"}
+        mock_post.assert_called_with(URI, auth=("user", "password"), data=data, headers=headers, verify=False)
 
     # @mock.patch.object(F5Device, "file_copy_remote_exists", side_effect=[True])
     # def test_file_copy_file_exists(self, mock_fcre):
