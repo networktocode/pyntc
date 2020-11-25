3f6bb7f270e385279b7084c0e7ee8ca5415384df
Dr. X <xenia.mountrouidou@networktocode.com>
2020-11-24 23:13:52 -0500

    Add try/except to handle ios ssh session management (#165)

--- pyntc/errors.py
+++ pyntc/errors.py
@@ -196,6 +196,21 @@ class NTCFileNotFoundError(NTCError):
         super().__init__(message)
 
 
+class SocketClosedError(NTCError):
+    default_message = (
+        "The device closed the connection during operation. Please make sure that you have remote access to the device."
+    )
+
+    def __init__(self, message=None):
+        """
+        Error for network device closing the socket connection during operation.
+
+        Args:
+            message (str): A custom error message to use instead of the default.
+        """
+        super().__init__(message or self.default_message)
+
+
 class WLANEnableError(NTCError):
     def __init__(self, hostname, desired_wlans, actual_wlans):
         """
