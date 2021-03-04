69a14bc42f4782decdbf5d2a0bb5ad9829065171
Adam Byczkowski <adam.byczkowski@networktocode.com>
2021-03-04 00:55:12 -0600

    Added docstrings to jnpr_device.py

--- pyntc/devices/jnpr_device.py
+++ pyntc/devices/jnpr_device.py
@@ -1,3 +1,4 @@
+"""MOdule for using a Juniper junOS device."""
 import os
 import re
 import time
@@ -25,7 +26,14 @@ class JunosDevice(BaseDevice):
 
     vendor = "juniper"
 
-    def __init__(self, host, username, password, *args, **kwargs):
+    def __init__(self, host, username, password, *args, **kwargs):  # noqa: D403
+        """PyNTC device implementation for Juniper junOS.
+
+        Args:
+            host (str): The address of the network device.
+            username (str): The username to authenticate with the device.
+            password (str): The password to authenticate with the device.
+        """
         super().__init__(host, username, password, *args, device_type="juniper_junos_netconf", **kwargs)
 
         self.native = JunosNativeDevice(*args, host=host, user=username, passwd=password, **kwargs)
@@ -103,17 +111,33 @@ class JunosDevice(BaseDevice):
         raise RebootTimeoutError(hostname=self.hostname, wait_time=timeout)
 
     def backup_running_config(self, filename):
+        """Backup current running configuration.
+
+        Args:
+            filename (str): Name used for backup file.
+        """
         with open(filename, "w") as f:
             f.write(self.running_config)
 
     @property
     def boot_options(self):
+        """Get os version on device.
+
+        Returns:
+            str: OS version on device.
+        """
         return self.os_version
 
     def checkpoint(self, filename):
+        """Create checkpoint file.
+
+        Args:
+            filename (str): Name of checkpoint file.
+        """
         self.save(filename)
 
     def close(self):
+        """Close connection to device."""
         if self.connected:
             self.native.close()
 
@@ -123,7 +147,7 @@ class JunosDevice(BaseDevice):
         Args:
              commands (str, list): String with single command, or list with multiple commands.
 
-         Raises:
+        Raises:
              ConfigLoadError: Issue with loading the command.
              CommandError: Issue with the command provided, if its a single command, passed in as a string.
              CommandListError: Issue with a command in the list provided.
@@ -156,10 +180,20 @@ class JunosDevice(BaseDevice):
 
     @property
     def connected(self):
+        """Get connection status of device.
+
+        Returns:
+            bool: True if connection is active. Otherwise, false.
+        """
         return self.native.connected
 
     @property
     def uptime(self):
+        """Get device uptime in seconds.
+
+        Returns:
+            int: Device uptime in seconds.
+        """
         try:
             native_uptime_string = self.native.facts["RE0"]["up_time"]
         except (AttributeError, TypeError):
@@ -173,6 +207,11 @@ class JunosDevice(BaseDevice):
 
     @property
     def uptime_string(self):
+        """Device uptime in string format ``days:hours:mins:secs``.
+
+        Returns:
+            str: Device uptime.
+        """
         try:
             native_uptime_string = self.native.facts["RE0"]["up_time"]
         except (AttributeError, TypeError):
@@ -185,6 +224,11 @@ class JunosDevice(BaseDevice):
 
     @property
     def hostname(self):
+        """Get device hostname.
+
+        Returns:
+            str: Device hostname.
+        """
         if self._hostname is None:
             self._hostname = self.native.facts.get("hostname")
 
@@ -192,6 +236,11 @@ class JunosDevice(BaseDevice):
 
     @property
     def interfaces(self):
+        """Get list of interfaces.
+
+        Returns:
+            list: List of interfaces.
+        """
         if self._interfaces is None:
             self._interfaces = self._get_interfaces()
 
@@ -199,6 +248,11 @@ class JunosDevice(BaseDevice):
 
     @property
     def fqdn(self):
+        """Get fully qualified domain name.
+
+        Returns:
+            str: Fully qualified domain na,e
+        """
         if self._fqdn is None:
             self._fqdn = self.native.facts.get("fqdn")
 
@@ -206,6 +260,11 @@ class JunosDevice(BaseDevice):
 
     @property
     def model(self):
+        """Get device model.
+
+        Returns:
+            str: Get device model.
+        """
         if self._model is None:
             self._model = self.native.facts.get("model")
 
@@ -213,6 +272,11 @@ class JunosDevice(BaseDevice):
 
     @property
     def os_version(self):
+        """Get OS version.
+
+        Returns:
+            str: OS version.
+        """
         if self._os_version is None:
             self._os_version = self.native.facts.get("version")
 
@@ -220,12 +284,26 @@ class JunosDevice(BaseDevice):
 
     @property
     def serial_number(self):
+        """Get serial number.
+
+        Returns:
+            str: Serial number.
+        """
         if self._serial_number is None:
             self._serial_number = self.native.facts.get("serialnumber")
 
         return self._serial_number
 
     def file_copy(self, src, dest=None, **kwargs):
+        """Copy file to device via SCP.
+
+        Args:
+            src (str): Name of file to be transferred.
+            dest (str, optional): Path on device to save file. Defaults to None.
+
+        Raises:
+            FileTransferError: Raised when unable to verify file was transferred succesfully.
+        """
         if not self.file_copy_remote_exists(src, dest, **kwargs):
             if dest is None:
                 dest = os.path.basename(src)
@@ -240,6 +318,15 @@ class JunosDevice(BaseDevice):
 
     # TODO: Make this an internal method since exposing file_copy should be sufficient
     def file_copy_remote_exists(self, src, dest=None, **kwargs):
+        """Verify device already has existing file.
+
+        Args:
+            src (str): Source of local file.
+            dest (str, optional): Path of file on device. Defaults to None.
+
+        Returns:
+            bool: True if hashes of the file match. Otherwise, false.
+        """
         if dest is None:
             dest = os.path.basename(src)
 
@@ -250,9 +337,18 @@ class JunosDevice(BaseDevice):
         return False
 
     def install_os(self, image_name, **vendor_specifics):
+        """Install OS on device.
+
+        Args:
+            image_name (str): Name of image.
+
+        Raises:
+            NotImplementedError: Method currently not implemented.
+        """
         raise NotImplementedError
 
     def open(self):
+        """Open connection to device."""
         if not self.connected:
             self.native.open()
 
@@ -275,6 +371,11 @@ class JunosDevice(BaseDevice):
         self.sw.reboot(in_min=timer)
 
     def rollback(self, filename):
+        """Rollback to a specific configuration file.
+
+        Args:
+            filename (str): Filename to rollback device to.
+        """
         self.native.timeout = 60
 
         temp_file = NamedTemporaryFile()
@@ -291,9 +392,22 @@ class JunosDevice(BaseDevice):
 
     @property
     def running_config(self):
+        """Get running configuration.
+
+        Returns:
+            str: Running configuration.
+        """
         return self.show("show config")
 
     def save(self, filename=None):
+        """Save current configuration to device. If filename is provided, save current configuration to file.
+
+        Args:
+            filename (str, optional): Filename to save current configuration. Defaults to None.
+
+        Returns:
+            bool: True if new file created for save file. Otherwise, just returns if save is to default name.
+        """
         if filename is None:
             self.cu.commit()
             return
@@ -309,6 +423,14 @@ class JunosDevice(BaseDevice):
         return True
 
     def set_boot_options(self, sys):
+        """Set boot options.
+
+        Args:
+            sys (str): Name of image to set boot option to.
+
+        Raises:
+            NotImplementedError: Method currently not implemented.
+        """
         raise NotImplementedError
 
     def show(self, commands):
@@ -339,6 +461,7 @@ class JunosDevice(BaseDevice):
 
     def show_list(self, commands, raw_text=True):
         """Send show commands in list format to a device.
+
         DEPRECATED - Use the `show` method.
         Args:
             commands (list): List with multiple commands.
@@ -348,4 +471,9 @@ class JunosDevice(BaseDevice):
 
     @property
     def startup_config(self):
+        """Get startup configuration.
+
+        Returns:
+            str: Startup configuration.
+        """
         return self.show("show config")
