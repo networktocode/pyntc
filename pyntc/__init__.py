79ad87bea2130a73f8353a36e52869bdd5e3fab9
Sheng <sheng-nong.tran_quyen@roche.com>
2022-09-01 13:48:01 +0200

    updated version for __init__ and pyproject

--- pyntc/__init__.py
+++ pyntc/__init__.py
@@ -11,7 +11,7 @@ try:
 except ImportError:
     from ConfigParser import SafeConfigParser
 
-__version__ = "0.20.1"
+__version__ = "0.20.2"
 
 LIB_PATH_ENV_VAR = "PYNTC_CONF"
 LIB_PATH_DEFAULT = "~/.ntc.conf"
