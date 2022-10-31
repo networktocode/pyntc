47e6db99eea1df71c4f40f0409a506dd131bd3b7
Armen Martirosyan <armartirosyan@gmail.com>
2022-10-30 21:28:02 -0700

    Fixes issue #213

--- pyntc/devices/ios_device.py
+++ pyntc/devices/ios_device.py
@@ -258,8 +258,7 @@ class IOSDevice(BaseDevice):
             except CommandError:
                 # Default to running config value
                 show_boot_out = self.show("show run | inc boot")
-                boot_path_regex = r"boot\s+system\s+(?:\S+\s+|)(\S+)\s*$"
-
+                boot_path_regex = r"boot\ssystem\s\S+(?::+|\s)(\S+.bin)"
         match = re.search(boot_path_regex, show_boot_out, re.MULTILINE)
         if match:
             boot_path_tuple = match.groups()
@@ -710,7 +709,10 @@ class IOSDevice(BaseDevice):
                         self.show(command, delay_factor=install_mode_delay_factor)
                     except IOError:
                         pass
-
+                    except CommandError:
+                        command = f"request platform software package install switch all file {self._get_file_system()}{image_name} auto-copy"
+                        self.show(command, delay_factor=install_mode_delay_factor)
+                        self.reboot()
             else:
                 self.set_boot_options(image_name, **vendor_specifics)
                 self.reboot()
@@ -721,7 +723,7 @@ class IOSDevice(BaseDevice):
             # Set FastCLI back to originally set when using install mode
             if install_mode:
                 self.fast_cli = current_fast_cli
-
+                image_name = INSTALL_MODE_FILE_NAME
             # Verify the OS level
             if not self._image_booted(image_name):
                 raise OSInstallError(hostname=self.hostname, desired_boot=image_name)
@@ -964,20 +966,54 @@ class IOSDevice(BaseDevice):
             CommandError: Error if setting new image as boot variable fails.
         """
         file_system = vendor_specifics.get("file_system")
+        command = "boot system flash"
         if file_system is None:
             file_system = self._get_file_system()
 
         file_system_files = self.show("dir {0}".format(file_system))
         if re.search(image_name, file_system_files) is None:
             raise NTCFileNotFoundError(hostname=self.hostname, file=image_name, dir=file_system)
-
-        try:
-            command = "boot system {0}/{1}".format(file_system, image_name)
+        if image_name == "packages.conf":
+            command = "boot system {0}{1}".format(file_system, image_name)
             self.config(["no boot system", command])
-        except CommandListError:  # TODO: Update to CommandError when deprecating config_list
-            file_system = file_system.replace(":", "")
-            command = "boot system {0} {1}".format(file_system, image_name)
-            self.config(["no boot system", command])
+        else:
+            show_boot_sys = self.show("show run | include boot system")
+            # Sample:
+            # boot system flash:/c3560-advipservicesk9-mz.122-44.SE.bin
+            # boot system flash0:/c3560-advipservicesk9-mz.122-44.SE.bin
+            if re.search(r"boot\ssystem\s\S+\:\/\S+", show_boot_sys):
+                command = "boot system {0}/{1}".format(file_system, image_name)
+                self.config(["no boot system", command])
+            # Sample:
+            # boot system flash:c3560-advipservicesk9-mz.122-44.SE.bin
+            # boot system flash0:c3560-advipservicesk9-mz.122-44.SE.bin
+            elif re.search(r"boot\ssystem\s\S+\:\S+", show_boot_sys):
+                command = "boot system {0}{1}".format(file_system, image_name)
+                self.config(["no boot system", command])
+            # Sample:
+            # boot system flash flash:c3560-advipservicesk9-mz.122-44.SE.bin
+            # boot system flash flash0:c3560-advipservicesk9-mz.122-44.SE.bin
+            # boot system flash bootflash:c3560-advipservicesk9-mz.122-44.SE.bin
+            elif re.search(
+                r"boot\ssystem\s\S+\s\S+:\S+", show_boot_sys
+            ):  # TODO: Update to CommandError when deprecating config_list
+                command = "boot system flash {0}{1}".format(file_system, image_name)
+                self.config(["no boot system", command])
+            # Sample:
+            # boot system flash c3560-advipservicesk9-mz.122-44.SE.bin
+            elif re.search(
+                r"boot\ssystem\sflash\s\S+", show_boot_sys
+            ):  # TODO: Update to CommandError when deprecating config_list
+                file_system = file_system.replace(":", "")
+                command = "boot system {0} {1}".format(file_system, image_name)
+                self.config(["no boot system", command])
+            else:
+                raise CommandError(
+                    command=command,
+                    message="Unable to determine the boot system configuration syntax. Current config is {0}".format(
+                        show_boot_sys
+                    ),
+                )
 
         self.save()
         new_boot_options = self.boot_options["sys"]
